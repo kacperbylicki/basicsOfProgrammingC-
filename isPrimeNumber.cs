76460b0e5ad8a5aa7327919b07da8e91a9ca35da
kacperbylicki <60986172+kacperbylicki@users.noreply.github.com>
2020-11-21 19:17:44 +0100

    Update isPrimeNumber.cs

--- isPrimeNumber.cs
+++ isPrimeNumber.cs
@@ -4,6 +4,17 @@ namespace BeforeExam
 {
     internal class Program
     {
+        private static uint TestParse(string number)
+        {
+            if (uint.TryParse(number, out uint numberUint) == false)
+            {
+                Console.WriteLine("Wrong Format -- Natural number expected");
+                Environment.Exit(0); 
+            }
+
+            return numberUint;
+        }
+        
         private static bool IsPrime(uint number)
         {
             if (number == 0 || number == 1 || number % 2 == 0) return false;
@@ -12,24 +23,24 @@ namespace BeforeExam
             uint boundary = (uint) Math.Floor(Math.Sqrt(number));
 
             for (uint i = 3; i <= boundary; i += 2)
-                if (number % i == 0)
-                    return false;
+            {
+                if (number % i == 0) return false;
+            }
 
             return true;
         }
 
         private static void Main(string[] args)
         {
-            Console.Write("Enter number: ");
+            Console.Write("Input number: ");
 
-            string number = Console.ReadLine();
-
+            uint numberUint = TestParse(Console.ReadLine());
+            
+            // Ternary operator syntax
             Console.WriteLine(
-                uint.TryParse(number, out uint numberUint) == false
-                    ? "Wrong Format -- Natural number expected"
-                    : IsPrime(numberUint)
-                        ? "Is prime number."
-                        : "Is not prime number."
+                IsPrime(numberUint) 
+                    ? $"{numberUint} is prime number." 
+                    : $"{numberUint} is not prime number."
             );
         }
     }
